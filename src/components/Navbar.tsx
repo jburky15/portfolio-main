addbc5afe9caea50af6d97c132931ae01507a4cd
jburky15 <jburky15@gmail.com>
2026-08-27 20:55:22 -0400

    Scaffolding...

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <nav>
+            <p>Joe Burkhart</p>
+
+            <div>
+                <a href="#projects">Projects</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
